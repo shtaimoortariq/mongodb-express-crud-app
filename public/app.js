9cd6e56e171836372fad9e24f5d270b1464c0090
taimoor123 <taimoor_t@yahoo.com>
2016-08-15 20:38:05 +0500

    Creat Update Delete Using MongoDb Is Complete

--- public/app.js
+++ public/app.js
@@ -7,15 +7,25 @@
         this.dataDb = [];
 
         this.submit = function () {
+            $http.post("/insert", _self.user).then(_self.getData(), _self.user = "");
             console.log(_self.user);
-            $http.post("/insert", _self.user).then(_self.getData())
         };
 
         this.getData = function () {
             $http.get("/getData")
                 .success(function (data) {
                     _self.dataDb = data;
-                })
-        }
+            })
+        };
+
+        this.updateUser = function () {
+            _self.updateUserData = Object.assign({ id: _self.UpdateUser.id }, _self.user);
+            $http.post("/updateData", _self.updateUserData).then(_self.getData(), _self.user = "", _self.UpdateUser = "");
+        };
+
+        this.deleteUser = function () {
+            
+            $http.post("/deleteData", _self.UpdateUser).then(_self.getData(), _self.UpdateUser = "");
+        };
     }
 })();
