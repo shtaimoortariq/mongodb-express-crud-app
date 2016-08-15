6cebad117a4ab20df01aa0666f613159f22f3518
taimoor123 <taimoor_t@yahoo.com>
2016-08-15 16:12:38 +0500

    The Get And Post Request Working Properly

--- public/app.js
+++ public/app.js
@@ -1,21 +1,21 @@
 (function () {
     angular.module("myApp", ['ngMaterial'])
-    .controller("myController", ['$http', myController]);
+        .controller("myController", ['$http', myController]);
 
     function myController($http) {
         var _self = this;
+        this.dataDb = [];
 
         this.submit = function () {
             console.log(_self.user);
-            $http.post("/insert", _self.user);
+            $http.post("/insert", _self.user).then(_self.getData())
         };
 
         this.getData = function () {
-            $http.get("/get-data")
+            $http.get("/getData")
                 .success(function (data) {
-                    console.log(data);
+                    _self.dataDb = data;
                 })
-
         }
     }
 })();
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express    = require("express");
+var express = require("express");
 var bodyParser = require("body-parser");
-var morgan     = require("morgan");
-var path 	   = require("path");
-var mongo      = require("mongodb");
-var assert     = require("assert");
+var morgan = require("morgan");
+var path = require("path");
+var mongo = require("mongodb");
+var assert = require("assert");
 
-var url 	   = 'mongodb://localhost:27017/testing';
+var url = 'mongodb://localhost:27017/testing';
 
-var app 	   = express();
-app.use(bodyParser.urlencoded({ extended: false}));
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
@@ -19,42 +19,40 @@ app.use(morgan("dev"));
 
 app.post("/insert", function (req, res) {
 	var data = {
-		title   : req.body.title,
-		content : req.body.content, 
-		author  : req.body.author 	
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author
 	}
 
-mongo.connect(url, function(err, db) {
-	assert.equal(null, err);
-	db.collection('user-data').insertOne(data, function(err, result) {
+	mongo.connect(url, function (err, db) {
 		assert.equal(null, err);
-		console.log('insert-data');
-		db.close();
+		db.collection('user-data').insertOne(data, function (err, result) {
+			assert.equal(null, err);
+			console.log('insert-data');
+			db.close();
+		})
 	})
 
-})
+});
 
-app.get("/get-data", function(req, res) {
+
+app.get("/getData", function (req, res) {
 	var data = [];
-	mongo.connect(url, function(err, db) {
+	mongo.connect(url, function (err, db) {
 		assert.equal(null, err);
 		var cursor = db.collection('user-data').find();
-		cursor.forEach(function(doc, err) {
+		cursor.forEach(function (doc, err) {
 			assert.equal(null, err);
 			data.push(doc);
-		}, function() {
+		}, function () {
 			db.close();
 			res.send(data);
 			console.log('data-received');
 		})
 	})
-
-})
-
-
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
 	console.log("server is at 3000");
 });
